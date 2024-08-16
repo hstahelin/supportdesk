9db5b43745797170bd5ad06f23abfcfa2461379d
HugoStahelin MacBookAir <hugo.stahelin@gmail.com>
2024-08-16 10:58:37 -0700

    Add tickets/getAll search by email

--- server/controllers/tickets-controller.js
+++ server/controllers/tickets-controller.js
@@ -2,7 +2,16 @@ const knex = require("knex")(require("../knexfile"));
 
 const getAll = async (req, res) => {
   try {
-    const tickets = await knex("tickets_current");
+    const { email } = req.query;
+
+    let query = knex("tickets_current");
+
+    if (email) {
+      query = query.where("created_email", email);
+    }
+    console.log(query);
+
+    const tickets = await query;
     res.status(200).json(tickets);
   } catch (err) {
     res.status(400).send(`Error retrieving tickets: ${err}`);
