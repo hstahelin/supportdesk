e712618d6767103e7e1bde78bdf7130982e83a39
HugoStahelin MacBookAir <hugo.stahelin@gmail.com>
2024-08-16 11:36:32 -0700

    Client setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.scss";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Support Desk</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
