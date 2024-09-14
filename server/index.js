932cd61be7acfeb9ba53f1510b2096d64e2df192
HugoStahelin MacBookAir <hugo.stahelin@gmail.com>
2024-09-14 14:14:09 -0700

    Fix session, use MySQL

--- server/index.js
+++ server/index.js
@@ -1,30 +1,61 @@
 const express = require("express");
 const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 const passport = require("./passport");
 const cors = require("cors");
 const app = express();
-
 require("dotenv").config();
+
+const options = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_LOCAL_USER,
+  password: process.env.DB_LOCAL_PASSWORD,
+  database: process.env.DB_LOCAL_DBNAME,
+};
+
+const sessionStore = new MySQLStore(options);
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Parse CORS_ORIGINS into an array
+const allowedOrigins = process.env.CORS_ORIGINS
+  ? process.env.CORS_ORIGINS.split(",")
+  : [];
+
 const corsOptions = {
-  origin: true,
+  origin: function (origin, callback) {
+    if (allowedOrigins.length === 0 || allowedOrigins.indexOf(origin) !== -1) {
+      // Allow the request
+      callback(null, true);
+    } else {
+      // Reject the request
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 };
+
+// const corsOptions = {
+//   origin: true,
+//   credentials: true,
+// };
+
 app.use(cors(corsOptions));
 
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "supportdesk_secret_key",
+    store: sessionStore,
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
-      httpOnly: true,
-      sameSite: "lax",
+      // secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      secure: false, // Use secure cookies in production
+      httpOnly: true, // Prevents JavaScript access to cookies
+      sameSite: "lax", // Provides some protection against CSRF
       maxAge: 3600000, // 1 hour
     },
   })
